732004344959e7049981c170c742dc4bea951042
Your Name <you@example.com>
2023-04-05 17:40:24 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :recipes
   resources :end_users
   resources :reviews
